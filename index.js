f2aead12ecbf6a1feefa4550712d75d52731213c
aldiwira <aldiwira.aw@gmail.com>
2021-10-18 21:41:50 +0700

    fix env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
