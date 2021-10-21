2b2e1570b2ff30b865bcf3e75f1edc3da247f4ab
aldiwira <aldiwira.aw@gmail.com>
2021-10-22 00:44:44 +0700

    remove unused

--- helper/index.js
+++ helper/index.js
@@ -3,7 +3,6 @@ const jwt = require('./jwt');
 const db = require('./db');
 const bcrypts = require('./bcrypt');
 const permission = require('./permission');
-const validator = require('./validator');
 const multer = require("./multer");
 
-module.exports = { response, jwt, db, bcrypts, permission, validator, multer };
+module.exports = { response, jwt, db, bcrypts, permission, multer };
